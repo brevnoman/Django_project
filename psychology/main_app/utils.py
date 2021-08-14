f346f22e1ac9894d8b02525c4c76fbb37e4859b1
Sautin Dmytro <dimonsova1488@gmail.com>
2021-08-14 16:19:56 +0300

    graduate_project

--- psychology/main_app/utils.py
+++ psychology/main_app/utils.py
@@ -1,3 +1,83 @@
-from main_app.models import Conclusion
+import datetime
 
-def conclusion_adder():
+from main_app.models import Conclusion, Meeting
+
+
+def calendar_util(year, month):
+    current_month_list = []
+    current_year = int(year)
+    current_month = int(month)
+    current_month_text = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+    first_day_of_current_month = datetime.datetime(year=current_year, month=current_month, day=1).weekday()
+    previous_month = (datetime.datetime(year=current_year, month=current_month, day=1)-datetime.timedelta(days=15)).month
+    previous_year = (datetime.datetime(year=current_year, month=current_month, day=1)-datetime.timedelta(days=15)).year
+    print(previous_month)
+    next_month = (datetime.datetime(year=current_year, month=current_month, day=27)+datetime.timedelta(days=15)).month
+    next_year = (datetime.datetime(year=current_year, month=current_month, day=27)+datetime.timedelta(days=15)).year
+    print(next_month)
+    month_for_now = datetime.datetime.today().month
+    year_for_now = datetime.datetime.today().year
+    first_week = True
+    day = 1
+    meetings = show_undone_meetings()
+    current_week = []
+    try:
+        for _ in range(6):
+            current_week = []
+            for i in range(7):
+                if first_week and i < first_day_of_current_month:
+                    current_week += "_"
+                else:
+                    meet_is_true = False
+                    needed_meet = ""
+                    for meet in meetings:
+                        if datetime.datetime(year=current_year, month=current_month, day=day).month == meet.date.month and datetime.datetime(year=current_year, month=current_month, day=day).day == meet.date.day:
+                            print(datetime.datetime(year=current_year, month=current_month, day=day).day)
+                            print(meet.date.day)
+                            print("kukusiki")
+                            meet_is_true = True
+                            needed_meet = meet
+                    if meet_is_true:
+                        html = f'<a href="/user_page/meetings_of_date/{needed_meet.date}">{str(needed_meet.date)[8:10]}</a>'
+                        current_week.append(html)
+                        meet_is_true = False
+                    else:
+                        if day < 10:
+                            current_week.append(f"0{day}")
+                        else:
+                            current_week.append(day)
+                    day += 1
+            first_week = False
+            current_month_list.append(current_week)
+    except ValueError:
+        for i in range(6 - datetime.datetime(year=current_year, month=current_month, day=day-1).weekday()):
+            current_week += "_"
+        current_month_list.append(current_week)
+    return {
+        "cal": current_month_list,
+        'year': year_for_now,
+        "month": month_for_now,
+        "previous_month": previous_month,
+        "next_month": next_month,
+        "previous_year":previous_year,
+        'next_year': next_year,
+        "current_month_text": current_month_text[current_month-1],
+        "now_year": current_year,
+        "now_month": current_month
+    }
+
+
+def show_undone_meetings():
+    meetings = Meeting.objects.filter(is_accepted=True, is_done=False).order_by("date").all()
+    return meetings
+
+
+def get_user_meetings(request):
+    user_meetings = Meeting.objects.filter(user=request.user, is_accepted=True).order_by("date").all()
+    return user_meetings
+
+
+def get_user_conclusions(meeting):
+    user_conclusions = Conclusion.objects.filter(meeting=meeting).all()
+    # print(user_conclusions)
+    return user_conclusions
--- psychology/main_app/views.py
+++ psychology/main_app/views.py
@@ -1,5 +1,5 @@
 import datetime
-
+from main_app.utils import calendar_util, show_undone_meetings, get_user_meetings, get_user_conclusions
 from django.shortcuts import render, redirect
 from main_app.forms import NewUserForm, MeetingCreateForm, NewUserChangeForm, User
 from django.contrib.auth import login, authenticate, logout
@@ -16,70 +16,8 @@ def date_meetings(request, meetings_date):
 
 #delete it
 def calendar(request, year, month):
-    current_month_list = []
-    current_year = int(year)
-    current_month = int(month)
-    current_month_text = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
-    first_day_of_current_month = datetime.datetime(year=current_year, month=current_month, day=1).weekday()
-    previous_month = (datetime.datetime(year=current_year, month=current_month, day=1)-datetime.timedelta(days=15)).month
-    previous_year = (datetime.datetime(year=current_year, month=current_month, day=1)-datetime.timedelta(days=15)).year
-    print(previous_month)
-    next_month = (datetime.datetime(year=current_year, month=current_month, day=27)+datetime.timedelta(days=15)).month
-    next_year = (datetime.datetime(year=current_year, month=current_month, day=27)+datetime.timedelta(days=15)).year
-    print(next_month)
-    month_for_now = datetime.datetime.today().month
-    year_for_now = datetime.datetime.today().year
-    first_week = True
-    day = 1
-    meetings = show_undone_meetings()
-    current_week = []
-    try:
-        for _ in range(6):
-            current_week = []
-            for i in range(7):
-                if first_week and i < first_day_of_current_month:
-                    current_week += "_"
-                else:
-                    meet_is_true = False
-                    needed_meet = ""
-                    for meet in meetings:
-                        if datetime.datetime(year=current_year, month=current_month, day=day).month == meet.date.month and datetime.datetime(year=current_year, month=current_month, day=day).day == meet.date.day:
-                            print(datetime.datetime(year=current_year, month=current_month, day=day).day)
-                            print(meet.date.day)
-                            print("kukusiki")
-                            meet_is_true = True
-                            needed_meet = meet
-                    if meet_is_true:
-                        html = f'<a href="/user_page/meetings_of_date/{needed_meet.date}">{str(needed_meet.date)[8:10]}</a>'
-                        current_week.append(html)
-                        meet_is_true = False
-                    else:
-                        if day < 10:
-                            current_week.append(f"0{day}")
-                        else:
-                            current_week.append(day)
-                    day += 1
-            first_week = False
-            current_month_list.append(current_week)
-    except ValueError:
-        for i in range(6 - datetime.datetime(year=current_year, month=current_month, day=day-1).weekday()):
-            current_week += "_"
-        current_month_list.append(current_week)
-    html_thing = '<a href="/user_page/meetings/"><h4>Открыть</h4></a>'
-    just_number = "5"
-    print(current_month_list)
-    return render(request, "Backup.html", context={"cal": current_month_list,
-                                                   "html_code": html_thing,
-                                                   'num': just_number,
-                                                   'year': year_for_now,
-                                                   "month": month_for_now,
-                                                   "previous_month": previous_month,
-                                                   "next_month": next_month,
-                                                   "previous_year":previous_year,
-                                                   'next_year': next_year,
-                                                   "current_month_text": current_month_text[current_month-1],
-                                                   "now_year": current_year,
-                                                   "now_month": current_month})
+    context = calendar_util(year, month)
+    return render(request, "calendar.html", context=context)
 
 
 def user_page(request):
@@ -176,16 +114,6 @@ def login_request(request):
     return render(request=request, template_name="login.html", context={"login_form": form})
 
 
-
-def get_user_meetings(request):
-    user_meetings = Meeting.objects.filter(user=request.user, is_accepted=True).order_by("date").all()
-    return user_meetings
-
-def get_user_conclusions(meeting):
-    user_conclusions = Conclusion.objects.filter(meeting=meeting).all()
-    # print(user_conclusions)
-    return user_conclusions
-
 def user_page_editing(request):
     if request.method == "POST":
         form = NewUserChangeForm(request.POST)
@@ -209,8 +137,3 @@ def user_page_editing(request):
     current_month = datetime.datetime.today().month
     return render(request=request, template_name="user_page_edit.html", context={"meeting_form": form, 'year': current_year, "month": current_month})
 
-
-
-def show_undone_meetings():
-    meetings = Meeting.objects.filter(is_accepted=True, is_done=False).order_by("date").all()
-    return meetings
