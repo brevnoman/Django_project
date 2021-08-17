bf76e9109bc72d331f5b6c21240319912ce4849f
Sautin Dmytro <dimonsova1488@gmail.com>
2021-08-17 15:34:14 +0300

    graduate_project

--- telegram_bot/controller.py
+++ telegram_bot/controller.py
@@ -1,26 +1,20 @@
 import datetime
 import time
-import schedule
 from telegram_bot.models import Meeting, Session, User
 
 
-def say_me_when_work(bot, update):
+def say_me_when(bot, update):
     session = Session()
     while True:
-        meetings = session.query(Meeting).filter(Meeting.is_accepted==True).all()
+        meetings = session.query(Meeting).filter(Meeting.is_accepted == True).all()
         print('hi')
         for meeting in meetings:
-            if str(datetime.datetime.now() + datetime.timedelta(days=1))[:13] == str(datetime.datetime.combine(meeting.date, meeting.time_start))[:13]:
-                text = f"Завтра у вас встреча с {session.query(User.first_name, User.last_name).filter(User.id==meeting.user_id).first()}  c {meeting.time_start} до {meeting.time_end}\n"
+            if str(datetime.datetime.now() + datetime.timedelta(days=1))[:13] == str(
+                    datetime.datetime.combine(meeting.date, meeting.time_start))[:13]:
+                text = f"Завтра у вас встреча с {session.query(User.first_name, User.last_name).filter(User.id == meeting.user_id).first()}  c {meeting.time_start} до {meeting.time_end}\n"
                 update.bot.send_message(chat_id=bot.effective_chat.id, text=text)
-            if str(datetime.datetime.now() + datetime.timedelta(hours=1))[:13] == str(datetime.datetime.combine(meeting.date, meeting.time_start))[:13]:
+            if str(datetime.datetime.now() + datetime.timedelta(hours=1))[:13] == str(
+                    datetime.datetime.combine(meeting.date, meeting.time_start))[:13]:
                 text = f"Через час у вас встреча с {session.query(User.first_name, User.last_name).filter(User.id == meeting.user_id).first()}  c {meeting.time_start} до {meeting.time_end}\n"
                 update.bot.send_message(chat_id=bot.effective_chat.id, text=text)
         time.sleep(3600)
-
-
-
-def say_me_when(bot, update):
-    say_me_when_work(bot, update)
-
-
