f2b93cafd8d5b0398ce51fef0a4254a8b905b24c
Sautin Dmytro <dimonsova1488@gmail.com>
2021-08-17 20:19:39 +0300

    graduate_project

--- telegram_bot/controller.py
+++ telegram_bot/controller.py
@@ -14,7 +14,7 @@ def say_me_when(bot, update):
                     datetime.datetime.combine(meeting.date, meeting.time_start))[:13]:
                 text = f"Завтра у вас встреча с {session.query(User.first_name, User.last_name).filter(User.id == meeting.user_id).first()}  c {meeting.time_start} до {meeting.time_end}\n"
                 update.bot.send_message(chat_id=bot.effective_chat.id, text=text)
-            if str(datetime.datetime.now() + datetime.timedelta(hours=1))[:13] == str(
+            elif str(datetime.datetime.now() + datetime.timedelta(hours=1))[:13] == str(
                     datetime.datetime.combine(meeting.date, meeting.time_start))[:13]:
                 text = f"Через час у вас встреча с {session.query(User.first_name, User.last_name).filter(User.id == meeting.user_id).first()}  c {meeting.time_start} до {meeting.time_end}\n"
                 update.bot.send_message(chat_id=bot.effective_chat.id, text=text)
